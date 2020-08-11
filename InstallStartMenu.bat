6778f4ef1d26abd235b52c8fa272a297a7363225
ParliamoDiPC <66361695+ParliamoDiPC@users.noreply.github.com>
2020-08-11 11:33:21 +0200

    Add files via upload

---
+++ InstallStartMenu.bat
@@ -0,0 +1,7 @@
+@echo off
+set /p path="Dove vuoi la cartella Start? "
+set /p name="Come vuoi chiamare la cartella Start? "
+mkdir "%path%\%name%.{4234d49b-0245-4df3-b780-3893943456e1}"
+cls 
+echo Fatto!
+pause
