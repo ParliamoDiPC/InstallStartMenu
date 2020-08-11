b3b057a9908b8f9946ddaeeb339593128163b21d
ParliamoDiPC <66361695+ParliamoDiPC@users.noreply.github.com>
2020-08-11 12:00:02 +0200

    Delete InstallStartMenu.bat

--- InstallStartMenu.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set /p path="Dove vuoi la cartella Start? "
-set /p name="Come vuoi chiamare la cartella Start? "
-mkdir "%path%\%name%.{4234d49b-0245-4df3-b780-3893943456e1}"
-cls 
-echo Fatto!
-pause
